7df45aa8cf25eaf4614cc50e05ecd4ba4360da16
Gideon Lee <gideonxlee@gmail.com>
2021-08-22 00:14:22 -0700

    Update vertex-shader.glsl
    Added Points Testing environment code.

--- src/assets/vertex-shader.glsl
+++ src/assets/vertex-shader.glsl
@@ -1,3 +1,9 @@
 void main() {
+  // Testing environment for THREE.Points()
+	//vec3 pos = position;
+	//vec4 mvPosition = modelViewMatrix * vec4(pos, 1.0);
+  //gl_PointSize = 300.0 * (1.0 / - mvPosition.z);
+  //gl_Position = projectionMatrix * mvPosition;
+  
 	gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
