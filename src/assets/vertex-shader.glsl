36c408cc99674a71ce8de2ccaf8429b6e2fcb9ac
Gideon Lee <gideonxlee@gmail.com>
2022-02-18 16:10:42 -0800

    Access UV Coordinates
    Allow access to coordinates are used to access the texture

--- src/assets/vertex-shader.glsl
+++ src/assets/vertex-shader.glsl
@@ -1,3 +1,5 @@
+varying vec2 vUv;
+
 void main() {
   // Testing environment for THREE.Points()
 	//vec3 pos = position;
@@ -5,5 +7,7 @@ void main() {
   //gl_PointSize = 300.0 * (1.0 / - mvPosition.z);
   //gl_Position = projectionMatrix * mvPosition;
   
+  // Allow access to the UV coordinates in texture
+  vUv = uv;
 	gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
